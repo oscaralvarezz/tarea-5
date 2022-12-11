107d5b05277c384313af4779735da0f50e1abbf1
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-12-11 11:29:38 +0100

    ej1.py

---
+++ ej1.py
@@ -0,0 +1,35 @@
+#Código para el reloj en tiempo real
+
+from datetime import datetime
+import tkinter as tk
+INTERVALO_REFRESCO_RELOJ = 300  #En milisegundos
+
+
+def obtener_hora_actual():
+    return datetime.now().strftime("%H:%M:%S")
+
+
+def refrescar_reloj():
+    print("Actualizando")
+    variable_hora_actual.set(obtener_hora_actual())
+    raiz.after(INTERVALO_REFRESCO_RELOJ, refrescar_reloj)
+
+
+raiz = tk.Tk()
+variable_hora_actual = tk.StringVar(raiz, value=obtener_hora_actual())
+raiz.etiqueta = tk.Label(
+    raiz, textvariable=variable_hora_actual, font=f"Consolas 60")
+raiz.etiqueta.pack(side="top")
+app = tk.Frame()
+raiz.title("Reloj con hora actual")
+refrescar_reloj()
+app.pack()
+app.mainloop()
+
+import time
+while True:
+    localtime = time.localtime()
+    result = time.strftime("%I : %M : %S %p", localtime)
+    print(result)
+    time.sleep(1)
+    break
